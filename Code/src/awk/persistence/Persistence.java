cd7ebc9b72031d297f07bdb21865e379fc85bd7f
Philip Dauwe <philip@pdauwe.de>
2014-11-23 21:57:35 +0100

    Persistence hinzugefügt

---
+++ Code/src/awk/persistence/Persistence.java
@@ -0,0 +1,63 @@
+package awk.persistence;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Persistence {
+	 
+	public static Connection getConnection(){
+		Connection aConnection = null;
+		try {
+			/*
+			 * Hier den Namen des JDBC-Treibers angeben. Damit der auch gefunden wird, muss
+			 * das jar-File in Eclipde unter 
+			 * "Project->Properties->Java Build Path->Libraries->Add External Jars" angegeben werden
+			 */
+			Class.forName ("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+		}
+		try {
+			/* 
+			 * Form: @Rechnername:Port, "Db-User", "Password"
+			 */
+			aConnection = 
+					DriverManager.getConnection (
+							// TODO: ip, benutzername und passwort
+							//"jdbc:oracle:thin:@131.173.92.216:1521:orcl", "STUD12", "NewYork99");
+							"jdbc:oracle:thin:@x.x.x.x:xxxx:orcl", "username", "passwort");
+		} catch (SQLException e) {
+				e.printStackTrace();
+		}
+		
+		return(aConnection);	
+	}
+	
+	public static void closeConnection(Connection aConnection){
+		try {
+			aConnection.close();
+		} catch (SQLException e) {
+				e.printStackTrace();
+		}	
+	}
+	
+	
+	public static void executeUpdateStatement(Connection aConnection, String aStatement) throws SQLException{
+		System.out.println(aStatement);
+		Statement stmt = aConnection.createStatement();
+		stmt.executeUpdate(aStatement);
+		
+	}
+	
+	public static ResultSet executeQueryStatement(Connection aConnection, String aStatement) throws SQLException{
+		ResultSet aResultSet = null;
+		System.out.println(aStatement);
+		Statement stmt = aConnection.createStatement();
+		aResultSet =  stmt.executeQuery(aStatement);
+		
+		return aResultSet;
+	}
+	
+}
