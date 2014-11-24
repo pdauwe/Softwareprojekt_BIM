7bfad896e3aee817f812b428c0a52a8bdf0f9dfa
Philip Dauwe <philip@pdauwe.de>
2014-11-24 17:45:23 +0100

    Managerklasse für statische Objekte initial angelegt

---
+++ Code/src/awk/usecase/impl/StatischeObjekteManager.java
@@ -0,0 +1,46 @@
+package awk.usecase.impl;
+
+import java.util.ArrayList;
+
+import awk.entity.internal.Dozent;
+import awk.entity.internal.Modul;
+import awk.entity.internal.Raum;
+import awk.entity.internal.Semester;
+import awk.entity.internal.Studiengang;
+
+
+/*
+ * Managerklasse die alle statischen Objekte zurück gibt.
+ * Statische Objekte sind all die Objekte, die nicht vom Benutzer geändert werden können.  
+ */
+public class StatischeObjekteManager {
+
+	public ArrayList<Dozent> getDozenten(){
+		//TODO: Implementierung
+		return null;
+	}
+	
+	public ArrayList<Modul> getModule(){
+		//TODO: Implementierung
+		return null;
+	}
+	
+	public ArrayList<Raum> getRaeume(){
+		//TODO: Implementierung
+		return null;
+	}
+	
+	public ArrayList<Semester> getSemester(){
+		//TODO: Implementierung
+		return null;
+	}
+	
+	public ArrayList<Studiengang> getStudiengaenge(){
+		//TODO: Implementierung
+		return null;
+	}
+	
+	
+	
+	
+}
