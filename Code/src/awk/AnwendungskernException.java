72be779da3bbc3318cb0d1a414c2dd3f8bce1067
Philip Dauwe <philip@pdauwe.de>
2014-11-24 16:48:46 +0100

    Exceptionsklassen hinzugefügt

---
+++ Code/src/awk/AnwendungskernException.java
@@ -0,0 +1,16 @@
+package awk;
+
+/*
+ * 
+ * Philip Dauwe
+ * 579407
+ * 
+ */
+public class AnwendungskernException extends Throwable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9027829030705689748L;
+
+}
---
+++ Code/src/awk/DatenhaltungsException.java
@@ -0,0 +1,16 @@
+package awk;
+
+/*
+ * 
+ * Philip Dauwe
+ * 579407
+ * 
+ */
+public class DatenhaltungsException extends Throwable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6202625005888650908L;
+
+}
