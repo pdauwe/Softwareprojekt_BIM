1b654efdde2d5e5cf483784a10430511864c315a
Philip Dauwe <philip@pdauwe.de>
2014-11-24 17:45:33 +0100

    Managerklasse für Stundenplan initial angelegt

---
+++ Code/src/awk/usecase/impl/StudenplanManager.java
@@ -0,0 +1,21 @@
+package awk.usecase.impl;
+
+import awk.entity.internal.Studiengang;
+import awk.entity.internal.Stundenplan;
+
+
+/*
+ * Managerklasse zur Verwaltung von Stundenplänen
+ */
+public class StudenplanManager {
+
+	public Stundenplan getStundenplan(Studiengang studiengang){
+		//TODO: Implementierung
+		return null;
+	}
+	
+	public boolean stundenplanSpeichern(Stundenplan stundenplan){
+		//TODO: Implementierung
+		return false;
+	}
+}
