b12ea24127a7e491aab550fb9abe8ef1b69e199f
Philip Dauwe <philip@pdauwe.de>
2014-11-24 16:49:22 +0100

    Stundenplan- & Studiengang-Klassen

---
+++ Code/src/awk/entity/Studiengang.java
@@ -0,0 +1,18 @@
+package awk.entity;
+
+public class Studiengang {
+
+	private String name;
+	
+	public Studiengang(String name){
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ Code/src/awk/entity/Stundenplan.java
@@ -0,0 +1,40 @@
+package awk.entity;
+
+import java.util.ArrayList;
+
+public class Stundenplan {
+
+	private ArrayList<ModulWrapper> module;
+	private Studiengang studiengang;
+	private Semester semester;
+	
+	public Stundenplan(Studiengang studiengang, Semester semester){
+		
+	}
+
+	public ArrayList<ModulWrapper> getModule() {
+		return module;
+	}
+
+	public void addModul(ModulWrapper modul) {
+		this.module.add(modul);
+	}
+
+	public Studiengang getStudiengang() {
+		return studiengang;
+	}
+
+	public void setStudiengang(Studiengang studiengang) {
+		this.studiengang = studiengang;
+	}
+
+	public Semester getSemester() {
+		return semester;
+	}
+
+	public void setSemester(Semester semester) {
+		this.semester = semester;
+	}
+	
+	
+}
