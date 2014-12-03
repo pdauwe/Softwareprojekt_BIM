2169aaa9cf7243fded8efd0e0f2bc3459884c87c
Philip Dauwe <philip@pdauwe.de>
2014-12-03 16:48:31 +0100

    StundenplanDatenzugriff erste Methodenplatzhalter angelegt

--- Code/src/awk/persistence/IStundenplanDatenzugriff.java
+++ Code/src/awk/persistence/IStundenplanDatenzugriff.java
@@ -1,5 +1,24 @@
 package awk.persistence;
 
+import java.util.ArrayList;
+
+import awk.DatenhaltungsException;
+import awk.entity.DozentTO;
+import awk.entity.ModulTO;
+import awk.entity.RaumTO;
+import awk.entity.StudiengangTO;
+import awk.entity.StundenplanTO;
+
 public interface IStundenplanDatenzugriff {
 
+	public ArrayList<DozentTO> alleDozenten() throws DatenhaltungsException;
+	public ArrayList<ModulTO> alleModule() throws DatenhaltungsException;
+	public ArrayList<StudiengangTO>  alleStudiengaenge() throws DatenhaltungsException;
+	public ArrayList<RaumTO> alleRaeume() throws DatenhaltungsException;
+	
+	public boolean speichereDozent(DozentTO dozent) throws DatenhaltungsException;
+
+	public boolean speichereStundenplan(StundenplanTO stundenplan) throws DatenhaltungsException;
+	public StundenplanTO ladeStundenplanFuerStudiengang(StudiengangTO studiengang) throws DatenhaltungsException;
+	
 }
--- Code/src/awk/persistence/impl/StundenplanDatenzugriff.java
+++ Code/src/awk/persistence/impl/StundenplanDatenzugriff.java
@@ -1,7 +1,63 @@
 package awk.persistence.impl;
 
+import java.util.ArrayList;
+
+import awk.DatenhaltungsException;
+import awk.entity.DozentTO;
+import awk.entity.ModulTO;
+import awk.entity.RaumTO;
+import awk.entity.StudiengangTO;
+import awk.entity.StundenplanTO;
 import awk.persistence.IStundenplanDatenzugriff;
 
 public class StundenplanDatenzugriff implements IStundenplanDatenzugriff {
+
+	@Override
+	public ArrayList<DozentTO> alleDozenten() throws DatenhaltungsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<ModulTO> alleModule() throws DatenhaltungsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<StudiengangTO> alleStudiengaenge()
+			throws DatenhaltungsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<RaumTO> alleRaeume() throws DatenhaltungsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean speichereDozent(DozentTO dozent)
+			throws DatenhaltungsException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean speichereStundenplan(StundenplanTO stundenplan)
+			throws DatenhaltungsException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public StundenplanTO ladeStundenplanFuerStudiengang(
+			StudiengangTO studiengang) throws DatenhaltungsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 	
 }
