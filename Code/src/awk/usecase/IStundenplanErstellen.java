4a517f449a4d4f709a6e59e97056da8bb0d2dfd5
Philip Dauwe <philip@pdauwe.de>
2014-12-03 16:47:57 +0100

    StundenplanErstellen Methodenplatzhalter hinzugefügt+
    initiale Methode für Urplanerstellung ebenfalls hinzugefügt

--- Code/src/awk/usecase/IStundenplanErstellen.java
+++ Code/src/awk/usecase/IStundenplanErstellen.java
@@ -9,4 +9,14 @@ public interface IStundenplanErstellen {
 
 	public Stundenplan stundenplanGenerieren(Studiengang studiengang, ArrayList<Dozent> dozenten, ArrayList<Modul> module) throws AnwendungskernException;
 	public boolean stundenplanSpeichern(Stundenplan stundenplan) throws AnwendungskernException;
+	
+	public ArrayList<Studiengang> generiereStudiengangZufallsliste() throws AnwendungskernException;
+	public Dozent randomDozentMitZeitpref(int zeitpref, Studiengang studiengang) throws AnwendungskernException;
+	public Dozent randomDozent(Studiengang studiengang) throws AnwendungskernException;
+	
+	public Modul randomModul(Dozent dozent, Studiengang studiengang);
+	
+	public Stundenplan erstelleUrplan() throws AnwendungskernException;
+	
+	
 }
--- Code/src/awk/usecase/impl/StundenplanErstellen.java
+++ Code/src/awk/usecase/impl/StundenplanErstellen.java
@@ -25,4 +25,95 @@ public class StundenplanErstellen implements IStundenplanErstellen {
 		return false;
 	}
 
+	@Override
+	public ArrayList<Studiengang> generiereStudiengangZufallsliste()
+			throws AnwendungskernException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Dozent randomDozentMitZeitpref(int zeitpref,
+			Studiengang studiengang) throws AnwendungskernException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Dozent randomDozent(Studiengang studiengang)
+			throws AnwendungskernException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Modul randomModul(Dozent dozent, Studiengang studiengang) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Stundenplan erstelleUrplan() throws AnwendungskernException {
+		
+		StundenplanManager stundenplanManager = StundenplanManager.getManager();
+		
+		ArrayList<Dozent> alleDozenten = DozentManager.getManager().getAlleDozenten();
+		ArrayList<Modul> alleModule = ModulManager.getManager().getAlleModule();
+		
+		
+		
+		for(int zeitslot = 0; zeitslot<15; zeitslot++){
+			
+			ArrayList<Studiengang> stList = this.generiereStudiengangZufallsliste();
+			
+			for(Studiengang s :stList){
+				Dozent randomDozent = this.randomDozentMitZeitpref(zeitslot, s);
+				
+				if(randomDozent == null){
+					break;
+				}
+				
+				Modul randomModul = this.randomModul(randomDozent, s);
+				
+				stundenplanManager.addToUrplan(zeitslot, s, randomModul, randomDozent);
+				
+				randomModul.setVerplant(true);
+				randomDozent.removeZeit(zeitslot);
+				
+				alleDozenten.set(alleDozenten.indexOf(randomDozent), randomDozent);
+				alleModule.set(alleModule.indexOf(randomModul), randomModul);
+			}
+		}
+		
+		if(stundenplanManager.isUrplanComplete()){
+			return stundenplanManager.getUrplan();
+		}
+		
+		for(int zeitslot = 0; zeitslot<15; zeitslot++){
+			
+			ArrayList<Studiengang> stList = this.generiereStudiengangZufallsliste();
+			
+			for(Studiengang s :stList){
+				Dozent randomDozent = this.randomDozent(s);
+				
+				if(randomDozent == null){
+					break;
+				}
+				
+				Modul randomModul = this.randomModul(randomDozent, s);
+				
+				stundenplanManager.addToUrplan(zeitslot, s, randomModul, randomDozent);
+				
+				randomModul.setVerplant(true);
+				randomDozent.removeZeit(zeitslot);
+				
+				alleDozenten.set(alleDozenten.indexOf(randomDozent), randomDozent);
+				alleModule.set(alleModule.indexOf(randomModul), randomModul);
+			}
+		}
+		
+		return stundenplanManager.getUrplan();
+		
+	}
+
 }
