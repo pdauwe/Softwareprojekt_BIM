6ec93c65ba3c2c9ffebb3ae1ddb9a66615c71b6c
Philip Dauwe <philip@pdauwe.de>
2014-12-06 14:26:21 +0100

    Laden der Dozenten, Module, Studiengänge aus der Datenbank

--- Code/src/awk/persistence/impl/StundenplanDatenzugriff.java
+++ Code/src/awk/persistence/impl/StundenplanDatenzugriff.java
@@ -1,5 +1,8 @@
 package awk.persistence.impl;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import awk.DatenhaltungsException;
@@ -8,27 +11,93 @@ import awk.entity.ModulTO;
 import awk.entity.RaumTO;
 import awk.entity.StudiengangTO;
 import awk.entity.StundenplanTO;
+import awk.persistence.DatenbankNamen;
 import awk.persistence.IStundenplanDatenzugriff;
 
 public class StundenplanDatenzugriff implements IStundenplanDatenzugriff {
 
 	@Override
 	public ArrayList<DozentTO> alleDozenten() throws DatenhaltungsException {
-		// TODO Auto-generated method stub
+		
+		ArrayList<DozentTO> alleDozenten = new ArrayList<DozentTO>();
+		
+		Connection aConnection = Persistence.getConnection();
+		ResultSet resultSet;
+		
+		try{
+			resultSet = Persistence.executeQueryStatement(aConnection, "SELECT * FROM " + DatenbankNamen.Dozent.Tabelle);
+			
+			while(resultSet.next()){
+				DozentTO dozent = new DozentTO();
+				dozent.setName(resultSet.getString(DatenbankNamen.Dozent.Name));
+				dozent.setZeiten(this.zeitPrefsFuerDozent(resultSet.getInt(DatenbankNamen.Dozent.DozentNummer)));
+				
+				alleDozenten.add(dozent);
+			}
+		}catch (SQLException e){
+			e.printStackTrace();
+			throw new DatenhaltungsException();
+		}finally{
+			Persistence.closeConnection(aConnection);
+		}
+		
 		return null;
 	}
 
 	@Override
 	public ArrayList<ModulTO> alleModule() throws DatenhaltungsException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		ArrayList<ModulTO> alleModule = new ArrayList<ModulTO>();
+		
+		Connection aConnection = Persistence.getConnection();
+		ResultSet resultSet;
+		
+		try{
+			resultSet = Persistence.executeQueryStatement(aConnection, "SELECT * FROM " + DatenbankNamen.Modul.Tabelle);
+			
+			while(resultSet.next()){
+				ModulTO modul = new ModulTO();
+				modul.setNummer(resultSet.getInt(DatenbankNamen.Modul.ModulNummer));
+				modul.setBenoetigtComputerraum( resultSet.getString(DatenbankNamen.Modul.BenoetigtComputerraum).charAt(0) == 'J' ? true : false);
+				modul.setVerplant(false);
+				
+				alleModule.add(modul);
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+			throw new DatenhaltungsException();
+		}finally{
+			Persistence.closeConnection(aConnection);
+		}
+		
+		return alleModule;
 	}
 
 	@Override
-	public ArrayList<StudiengangTO> alleStudiengaenge()
-			throws DatenhaltungsException {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<StudiengangTO> alleStudiengaenge() throws DatenhaltungsException {
+		
+		ArrayList<StudiengangTO> alleStudiengaenge = new ArrayList<StudiengangTO>();
+		
+		Connection aConnection = Persistence.getConnection();
+		ResultSet resultSet;
+		
+		try{
+			resultSet = Persistence.executeQueryStatement(aConnection, "SELECT * FROM " + DatenbankNamen.Studiengang.Tabelle);
+			
+			while(resultSet.next()){
+				StudiengangTO studiengang = new StudiengangTO();
+				studiengang.setName(resultSet.getString(DatenbankNamen.Studiengang.Name));
+				
+				alleStudiengaenge.add(studiengang);
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+			throw new DatenhaltungsException();
+		}finally{
+			Persistence.closeConnection(aConnection);
+		}
+		
+		return alleStudiengaenge;
 	}
 
 	@Override
@@ -58,6 +127,30 @@ public class StundenplanDatenzugriff implements IStundenplanDatenzugriff {
 		return null;
 	}
 	
+	/*
+	 *  Helpers
+	 */
 	
+	
+	private ArrayList<Integer> zeitPrefsFuerDozent(int dozentNummer) throws DatenhaltungsException{
+		ArrayList<Integer> zeitPrefs = new ArrayList<Integer>();
+		
+		Connection aConnection = Persistence.getConnection();
+		ResultSet resultSet;
+		
+		try{
+			resultSet = Persistence.executeQueryStatement(aConnection, "SELECT * FROM " + DatenbankNamen.ZeitpraeferenzDozentZuordnung.Tabelle + " WHERE" + DatenbankNamen.ZeitpraeferenzDozentZuordnung.DozentNummer + " = " + dozentNummer);
+			while(resultSet.next()){
+				zeitPrefs.add(resultSet.getInt(DatenbankNamen.ZeitpraeferenzDozentZuordnung.Zeitslot));
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+			throw new DatenhaltungsException();
+		}finally{
+			Persistence.closeConnection(aConnection);
+		}
+		
+		return zeitPrefs;
+	}
 	
 }
