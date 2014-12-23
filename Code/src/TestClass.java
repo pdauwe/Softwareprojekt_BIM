9ee82d3617c89f112ab8b40e6bd7ac9a532acaad
Philip Dauwe <philip@pdauwe.de>
2014-12-23 16:03:41 +0100

    Ausgabe Testklasse

--- Code/src/TestClass.java
+++ Code/src/TestClass.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import awk.AnwendungskernException;
 import awk.entity.internal.Studiengang;
 import awk.entity.internal.Stundenplan;
+import awk.entity.internal.StundenplanSlot;
 import awk.usecase.IStundenplanErstellen;
 import awk.usecase.impl.StundenplanErstellen;
 
@@ -20,8 +21,20 @@ public class TestClass {
 		try {
 			HashMap<Studiengang, Stundenplan> urplan = se.erstelleUrplan();
 			
-			System.out.println(urplan);
-			
+			for(Studiengang studiengang : urplan.keySet()){
+				System.out.println("############");
+				System.out.println("Studiengang: " + studiengang.getName());
+				System.out.println("############");
+				
+				
+				for(Integer i : urplan.get(studiengang).getZuordnung().keySet()){
+					System.out.println("Slot:" + i);
+					System.out.println("Dozent: " + urplan.get(studiengang).getZuordnung().get(i).getDozent().getName());
+					System.out.println("Modul: " + urplan.get(studiengang).getZuordnung().get(i).getModul().getBezeichnung());
+				}
+
+			}
+
 		} catch (AnwendungskernException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
